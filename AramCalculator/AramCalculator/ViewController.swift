73a6c57e2fa1610927b6bc7442c4805e39e5fc88
Minju <mlnjv016@gmail.com>
2022-09-09 15:07:31 +0900

    Project init

---
+++ AramCalculator/AramCalculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AramCalculator
+//
+//  Created by 박민주 on 2022/09/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
