c4c266e8397a00fa1cd6b44f25246fac77e097d3
Minju Park <mlnjv016@gmail.com>
2022-10-04 00:49:36 +0900

    [Feat] 낭비되는 돈 없을 경우의 이미지 추가 및 출력 기능 (#13)

--- AramCalculator/AramCalculator/ViewController.swift
+++ AramCalculator/AramCalculator/ViewController.swift
@@ -68,7 +68,7 @@ class ViewController: UIViewController {
         var textAlternativeFood = "" // 최종 출력 텍스트
         var imgAlternativeFood = "" // 최종 출력 이미지
         // 가격 비교
-        if leftFoodPrice < 3600 { textAlternativeFood = "낭비되는 돈이 없어요 :)"; imgAlternativeFood = "smiley" }
+        if leftFoodPrice < 3600 { textAlternativeFood = "낭비되는 돈이 없어요 :)"; imgAlternativeFood = "good" }
         else if leftFoodPrice < 6000 { textAlternativeFood = "커피 1잔을 마실 수 있어요."; imgAlternativeFood = "coffee" }
         else if leftFoodPrice < 20000 { textAlternativeFood = "국밥 \(leftFoodPrice / 6000)그릇을 먹을 수 있어요."; imgAlternativeFood = "riceSoup" }
         else { textAlternativeFood = "치킨 \(leftFoodPrice / 20000)마리를 먹을 수 있어요."; imgAlternativeFood = "chicken" }
